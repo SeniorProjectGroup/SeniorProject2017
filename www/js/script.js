b4c0030892dcc8a1e4aa3ce12b9d30812562e3d6
Andrew Wichmann <andyp.wichmann@gmail.com>
2017-04-21 15:01:36 -0500

    Fixing get location on android (#2)
    * Android get location works now, and added stop button functionality.
    
    * Added new rule to .gitignore for apks
    
    * Removed some debug messages

--- www/js/script.js
+++ www/js/script.js
@@ -1,22 +1,31 @@
-$(document).ready(function(){
+document.addEventListener("deviceready", onDeviceReady, false);
 
-  $('#start').click(function(){navigator.geolocation.watchPosition(onSuccess, onError)});
+function onDeviceReady() {
+    var watchID;
+    $('#start').click(function(){
+      watchID = navigator.geolocation.watchPosition(onSuccess, onError, { maximumAge: 3000, timeout: 10000, enableHighAccuracy: true  });
+    });
+    $('#stop').click(function(){
+      if(typeof watchID !== "undefined"){
+        navigator.geolocation.clearWatch(watchID);
+      };
+    })
 
-    var onSuccess = function(position) {
-        console.log('Latitude: '          + position.coords.latitude          + '\n' +
-              'Longitude: '         + position.coords.longitude         + '\n' +
-              'Altitude: '          + position.coords.altitude          + '\n' +
-              'Accuracy: '          + position.coords.accuracy          + '\n' +
-              'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-              'Heading: '           + position.coords.heading           + '\n' +
-              'Speed: '             + position.coords.speed             + '\n' +
-              'Timestamp: '         + position.timestamp                + '\n');
-    };
+  var onSuccess = function(position) {
+      console.log('Latitude: '          + position.coords.latitude          + '\n' +
+            'Longitude: '         + position.coords.longitude         + '\n' +
+            'Altitude: '          + position.coords.altitude          + '\n' +
+            'Accuracy: '          + position.coords.accuracy          + '\n' +
+            'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+            'Heading: '           + position.coords.heading           + '\n' +
+            'Speed: '             + position.coords.speed             + '\n' +
+            'Timestamp: '         + position.timestamp                + '\n');
+  };
 
-    // onError Callback receives a PositionError object
-    //
-    function onError(error) {
-        console.log('code: '    + error.code    + '\n' +
-              'message: ' + error.message + '\n');
-    }
-})
+  // onError Callback receives a PositionError object
+  //
+  function onError(error) {
+      console.log('code: '    + error.code    + '\n' +
+            'message: ' + error.message + '\n');
+  }
+}
